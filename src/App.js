6ca29fbabaaefaf3238306e9830188bece66a7f3
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 19:34:28 +0400

    made some changes

--- src/App.js
+++ src/App.js
@@ -2,35 +2,31 @@ import React, { useEffect } from "react";
 
 const MessengerChat = () => {
   useEffect(() => {
-    const loadSDK = () => {
-      if (window.FB) {
-        window.FB.XFBML.parse();
-        return;
-      }
+    // Check if the Facebook SDK is already present
+    if (window.FB) {
+      window.FB.XFBML.parse();
+      return;
+    }
 
-      const script = document.createElement("script");
-      script.id = "facebook-jssdk";
-      script.src =
-        "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      script.async = true;
-      script.onload = () => {
-        window.FB.init({
-          appId: "1131104729026921",
-          autoLogAppEvents: true,
-          xfbml: true,
-          version: "v10.0",
-        });
-        window.FB.XFBML.parse();
-      };
-
-      script.onerror = (error) => {
-        console.error("Error loading the Facebook SDK:", error);
-      };
-
-      document.body.appendChild(script);
+    // Load the Facebook SDK dynamically
+    const script = document.createElement("script");
+    script.id = "facebook-jssdk";
+    script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    script.async = true;
+    script.onload = () => {
+      window.FB.init({
+        appId: "1131104729026921", // New Facebook App ID
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v10.0",
+      });
+      window.FB.XFBML.parse();
+    };
+    script.onerror = (error) => {
+      console.error("Error loading the Facebook SDK:", error);
     };
 
-    loadSDK();
+    document.body.appendChild(script);
   }, []);
 
   return (
@@ -39,7 +35,7 @@ const MessengerChat = () => {
       <div
         className="fb-customerchat"
         attribution="setup_tool"
-        page_id="576324402235215"
+        page_id="576324402235215" // Your Facebook Page ID
         theme_color="#0084ff"
         logged_in_greeting="Hi! How can we help you?"
         logged_out_greeting="Goodbye! Come back soon."

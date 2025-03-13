c8bcf86839f8cbe2e4eb7d26c72a5ceb663a97f4
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 19:06:34 +0400

    messenger chatbot added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FacebookProvider appId="2126083514486740" chatSupport>
+        <CustomChat pageId="576324402235215" minimized="true" />
+      </FacebookProvider>
     </div>
   );
 }

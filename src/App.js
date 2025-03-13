ae1bd9c525a92b9b008869dcf47ca830defccd4e
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 19:16:07 +0400

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { FacebookProvider, CustomChat } from "react-facebook";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div>
-      <FacebookProvider appId="2126083514486740" chatSupport>
-        <CustomChat pageId="576324402235215" minimized="true" />
-      </FacebookProvider>
+      <MessengerCustomerChat
+        pageId="576324402235215"
+        appId="2126083514486740"
+      />
     </div>
   );
 }

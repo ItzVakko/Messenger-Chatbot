6ada1f50e2a633d76498cdbe298da3cf36f9fc3e
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 19:28:30 +0400

    changed messenger chatbot system

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
+import React, { useEffect } from "react";
 
-function App() {
+const MessengerChat = () => {
+  useEffect(() => {
+    const loadSDK = () => {
+      if (window.FB) {
+        window.FB.XFBML.parse();
+        return;
+      }
+
+      const script = document.createElement("script");
+      script.id = "facebook-jssdk";
+      script.src =
+        "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      script.async = true;
+      script.onload = () => {
+        window.FB.init({
+          appId: "1131104729026921",
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: "v10.0",
+        });
+        window.FB.XFBML.parse();
+      };
+
+      script.onerror = (error) => {
+        console.error("Error loading the Facebook SDK:", error);
+      };
+
+      document.body.appendChild(script);
+    };
+
+    loadSDK();
+  }, []);
+
   return (
     <div>
-      <MessengerCustomerChat
-        pageId="576324402235215"
-        appId="2126083514486740"
+      {/* The Facebook Messenger chat widget */}
+      <div
+        className="fb-customerchat"
+        attribution="setup_tool"
+        page_id="576324402235215"
+        theme_color="#0084ff"
+        logged_in_greeting="Hi! How can we help you?"
+        logged_out_greeting="Goodbye! Come back soon."
       />
     </div>
   );
-}
+};
 
-export default App;
+export default MessengerChat;
